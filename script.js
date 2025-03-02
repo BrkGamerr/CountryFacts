78cde37e5f864dfb8241961e806aa88c1f49d1f5
Brk <brkgamerr@gmail.com>
2025-03-02 23:37:38 +0100

    Added search bar, moved all <div> elements from HTML to .json (not finished).

--- script.js
+++ script.js
@@ -1,10 +1,38 @@
-const heroButton = document.getElementById("hero-button");
+const heroButton = document.getElementById("exploreButton");
+const countryCardTemplate = document.querySelector("[data-countries-template]");
+const countryCardContainer = document.querySelector("[data-country-container]");
+const searchInput = document.querySelector("[data-search]")
+
+let countries = []
 
 heroButton.addEventListener("click", heroButtonClick)
 
 
 function heroButtonClick() {
-	document.getElementById("search-div").scrollIntoView({behavior: "smooth"});
+	document.getElementById("searchDiv").scrollIntoView({behavior: "smooth"});
 }
 
-/* Maybe try to save all your card data to .json file, if it's easier to make search bar that way. */
+
+searchInput.addEventListener("input", e => {
+	const value = e.target.value.toLowerCase();
+	countries.forEach(country => {
+		const isVisible =
+			country.name.toLowerCase().includes(value);
+		country.element.classList.toggle("hide", !isVisible)
+	})
+})
+
+
+fetch("data.json")
+	.then(response => response.json())
+	.then(data => {
+		countries = data.map(country => {
+			const card = countryCardTemplate.content.cloneNode(true).children[0];
+			const image = card.querySelector("[data-image]");
+			const countryName = card.querySelector("[data-country-name]");
+			image.src = `flags/${country.name}.png`;
+			countryName.textContent = country.name;
+			countryCardContainer.append(card);
+			return {name: country.name, element: card}
+		})
+	})
